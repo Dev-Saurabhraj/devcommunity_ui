c4afd7a99ea50f8808c80efaa10110eda1517c66
saurabh <saurabhraj2509@gmail.com>
2026-05-04 23:30:02 +0530

    bloc implementtaion

---
+++ lib/shared/bottomNavigationBar/bloc/appBottomBarBloc.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'appBottomBarEvent.dart';
+import 'appBottomBarState.dart';
+
+class AppBottomBarBloc extends Bloc<AppBottomBarEvent, AppBottomBarState>{
+  AppBottomBarBloc(super.initialState);
+
+}
+
+
+
+
+
+
+
+
